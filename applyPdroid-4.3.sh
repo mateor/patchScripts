3513841b300085d457ffff01c1611eda66232740
Mateor <mateorod9@gmail.com>
2013-10-26 23:51:10 -0500

    Update applyPdroid-4.3.sh
    It now (finally) logs and reports patching failures. It is also configurable
    for individual user's setups and file systems. Vastly more portable this way.
    
    I have long needed the error-reporting, the embarassment of uploading such
    raunchy scripts has already paid off!

--- applyPdroid-4.3.sh
+++ applyPdroid-4.3.sh
@@ -2,26 +2,64 @@
 
 # Apply PDroid from build system Root.
 
+# This LOCK file is used by various scripts to determine if the patches are applied to the source code
 LOCK=~/android/openpdroid/.pdroid-lock
+
+# Change to whatever works for you
+ANDROID_HOME=~/android/system/jellybean
+PATCHES_LOCATION=~/android/OpenPDroidPatches
+BRANCH=4.3
+LOG_DIR=~/android/openpdroid
+LOG="$LOG_DIR"/SourcePatch.log
+
+print_error() {
+     echo ""
+     echo "!!! error: $@"
+     echo "At least one patch failed to apply! You can see the problem below and in $LOG"
+     echo ""
+     grep FAILED "$LOG"
+     echo ""
+     echo "These failures are often just import statements"
+     echo "and are generally easy to resolve manually."
+     echo ""
+     echo "If there is only one or two, I recommend you take a peek at the .rej file."
+     echo ""
+     echo "The directory the failure is in is listed above!"
+}
+
+patch_error() {
+     echo "!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!"
+     echo "!!! error: $@"
+     echo "!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!"
+     exit
+}
 
 if [ -f "$LOCK" ]; then
     echo ""
     echo "Pdroid patches are already applied."
-    echo "   removePdroid.sh should be ran to remov the lock at $LOCK"
+    echo "   removePdroid.sh should be ran to remove the lock at $LOCK"
     echo ""
     exit
 fi
 
-#TODO I should change this to provide a message and fail if patch fails.
-cd ~/android/Open*
-git checkout 4.3
+cd "$PATCHES_LOCATION"
+git checkout "$BRANCH" || patch_error "Could not checkout the branch you designated, double-check location and branch!"
 
-cd ~/android/system/jellybean/build; git checkout -b pdroid; patch -p1 < ~/android/Open*/openpdroid_4.3_build.patch
-cd ~/android/system/jellybean/libcore; git checkout -b pdroid; patch -p1 < ~/android/Open*/openpdroid_4.3_libcore.patch
-cd ~/android/system/jellybean/packages/apps/Mms; git checkout -b pdroid; patch -p1 < ~/android/Open*/openpdroid_4.3_packages_apps_Mms.patch
-cd ~/android/system/jellybean/frameworks/base; git checkout -b pdroid; patch -p1 < ~/android/Open*/openpdroid_4.3_frameworks_base.patch
-cd ~/android/system/jellybean/frameworks/opt/telephony; git checkout -b pdroid; patch -p1 < ~/android/Open*/openpdroid_4.3_frameworks_opt_telephony.patch
-cd ~/android/system/jellybean; . build/envsetup.sh
+DIRECTORIES_TO_PATCH=( build libcore frameworks_base frameworks_opt_telephony packages_apps_Mms )
+
+cd "$ANDROID_HOME"/build; git checkout -b pdroid; patch -p1 < "$PATCHES_LOCATION"/openpdroid_"$BRANCH"_build.patch >> "$LOG_DIR"/${DIRECTORIES_TO_PATCH[0]}.log 2>&1
+cd "$ANDROID_HOME"/libcore; git checkout -b pdroid; patch -p1 < "$PATCHES_LOCATION"/openpdroid_"$BRANCH"_libcore.patch >> "$LOG_DIR"/${DIRECTORIES_TO_PATCH[1]}.log 2>&1
+cd "$ANDROID_HOME"/frameworks/base; git checkout -b pdroid; patch -p1 < "$PATCHES_LOCATION"/openpdroid_"$BRANCH"_frameworks_base.patch >> "$LOG_DIR"/${DIRECTORIES_TO_PATCH[2]}.log 2>&1
+cd "$ANDROID_HOME"/frameworks/opt/telephony; git checkout -b pdroid; patch -p1 < "$PATCHES_LOCATION"/openpdroid_"$BRANCH"_frameworks_opt_telephony.patch >> "$LOG_DIR"/${DIRECTORIES_TO_PATCH[3]}.log 2>&1
+cd "$ANDROID_HOME"/packages/apps/Mms; git checkout -b pdroid; patch -p1 < "$PATCHES_LOCATION"/openpdroid_"$BRANCH"_packages_apps_Mms.patch >> "$LOG_DIR"/${DIRECTORIES_TO_PATCH[4]}.log 2>&1
+
+for d in ${DIRECTORIES_TO_PATCH[@]}; do
+     [[ $(grep FAILED "$LOG_DIR"/"$d".log) != "" ]] && print_error "Failure in $d!"
+     cat "$LOG_DIR"/"$d".log >> "$LOG"
+     rm "$LOG_DIR"/"$d".log
+done
+
+cd "$ANDROID_HOME"; . build/envsetup.sh
 
 # to determine if Pdroid patches are applied or not
 touch $LOCK
