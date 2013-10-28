7d14f4595d4210fed58c5d7ade37f960e550a855
Mateor <mateorod9@gmail.com>
2013-10-27 22:48:07 -0500

    applyPdroid.sh is now universal for all branches of OpD
    The script will take the branch as it's sole parameter, or default to latest
    version of OpD (4.3 currently). Detects failed patches and creates a log.
    
    Looks like it should do what we need.

--- applyPdroid.sh
+++ applyPdroid.sh
@@ -24,25 +24,97 @@
 
 # Apply PDroid from build system Root.
 
-LOCK=~/android/openpdroid/.pdroid-lock
+# Change to whatever works for you
+ANDROID_HOME=~/android/system/jellybean
+PATCHES_LOCATION=~/android/OpenPDroidPatches
+
+# TODO: make BRANCH a parameter
+BRANCH=4.3
+LOG_DIR=~/android/openpdroid
+
+LOG="$LOG_DIR"/OpDPatch.log
+# This LOCK file is used by various scripts to determine if the patches are applied to the source code
+LOCK="$LOG_DIR"/.pdroid-lock
+
+print_error() {
+     echo ""
+     echo "!!! error: $@"
+     echo "At least one patch failed to apply! You can see the problem below and in $LOG"
+     echo ""
+     grep FAILED "$LOG"
+     echo ""
+     echo "These failures are often just import statements"
+     echo "and are generally easy to resolve manually."
+     echo ""
+     echo "If there is only one or two, I recommend you take a peek at the .rej file."
+     echo ""
+}
+
+patch_error() {
+     echo "!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!"
+     echo "!!! error: $@"
+     echo "!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!"
+     exit
+}
+
+if [ $# -gt 0 ]; then
+     BRANCH=$1
+elif [ $# -gt 1 ]; then
+     patch_error "Only the OpenPDroidPatches branch name is accepted as a parameter!"
+fi
+
+AVAILABLE_BRANCHES=( 4.3 4.2.x 4.1.2-aosp 4.1.2-cm 4.1.1-aosp 4.1.1-cm 4.0.4-aosp 4.0.4-cm 2.3 )
+VALID_BRANCH=false
+for a in ${AVAILABLE_BRANCHES[@]}; do
+     if [[ "$a" == $BRANCH ]]; then
+          VALID_BRANCH=true
+     fi
+done
+
+if [[ ! "$VALID_BRANCH" == true ]]; then
+     patch_error "$BRANCH is not a valid branch! Check OpenPDroidPatch branch names for valid parameters!"
+fi
 
 if [ -f "$LOCK" ]; then
     echo ""
     echo "Pdroid patches are already applied."
-    echo "   removePdroid.sh should be ran to remov the lock at $LOCK"
+    echo "   removePdroid.sh should be ran to remove the lock at $LOCK"
     echo ""
     exit
 fi
 
-cd ~/android/open*/Open*
-git checkout 4.2.x
+cd "$PATCHES_LOCATION"
+git checkout "$BRANCH" || patch_error "Could not checkout the branch you designated, double-check location and branch!"
 
-cd ~/android/system/jellybean/build; git checkout -b pdroid; patch -p1 < ~/android/open*/Open*/openpdroid_4.2_build.patch
-cd ~/android/system/jellybean/libcore; git checkout -b pdroid; patch -p1 < ~/android/open*/Open*/openpdroid_4.2_libcore.patch
-cd ~/android/system/jellybean/packages/apps/Mms; git checkout -b pdroid; patch -p1 < ~/android/open*/Open*/openpdroid_4.2_packages_apps_Mms.patch
-cd ~/android/system/jellybean/frameworks/base; git checkout -b pdroid; patch -p1 < ~/android/open*/Open*/openpdroid_4.2_frameworks_base.patch
-cd ~/android/system/jellybean/frameworks/opt/telephony; git checkout -b pdroid; patch -p1 < ~/android/open*/Open*/openpdroid_4.2_frameworks_opt_telephony.patch
-cd ~/android/system/jellybean; . build/envsetup.sh
+DIRECTORIES_TO_PATCH=( build libcore frameworks_base packages_apps_Mms frameworks_opt_telephony )
+
+cd "$ANDROID_HOME"/build; git checkout -b pdroid; patch -p1 < "$PATCHES_LOCATION"/openpdroid_"$BRANCH"_build.patch >> "$LOG_DIR"/${DIRECTORIES_TO_PATCH[0]}.log 2>&1
+cd "$ANDROID_HOME"/libcore; git checkout -b pdroid; patch -p1 < "$PATCHES_LOCATION"/openpdroid_"$BRANCH"_libcore.patch >> "$LOG_DIR"/${DIRECTORIES_TO_PATCH[1]}.log 2>&1
+cd "$ANDROID_HOME"/frameworks/base; git checkout -b pdroid; patch -p1 < "$PATCHES_LOCATION"/openpdroid_"$BRANCH"_frameworks_base.patch >> "$LOG_DIR"/${DIRECTORIES_TO_PATCH[2]}.log 2>&1
+
+# Mms patch introduced in 4.1
+if [ -f $PATCHES_LOCATION/*"$BRANCH"*packages_apps_Mms*h ]; then
+     cd "$ANDROID_HOME"/packages/apps/Mms; git checkout -b pdroid; patch -p1 < "$PATCHES_LOCATION"/openpdroid_"$BRANCH"_packages_apps_Mms.patch >> "$LOG_DIR"/${DIRECTORIES_TO_PATCH[3]}.log 2>&1
+else
+     DIRECTORIES_TO_PATCH=( ${DIRECTORIES_TO_PATCH[@]//packages_apps_Mms} )
+fi
+
+# Telephony-common introduced in 4.2
+if [ -f $PATCHES_LOCATION/*"$BRANCH"*frameworks_opt_telephony*h ]; then
+     cd "$ANDROID_HOME"/frameworks/opt/telephony; git checkout -b pdroid; patch -p1 < "$PATCHES_LOCATION"/openpdroid_"$BRANCH"_frameworks_opt_telephony.patch >> "$LOG_DIR"/${DIRECTORIES_TO_PATCH[4]}.log 2>&1
+else
+     DIRECTORIES_TO_PATCH=( ${DIRECTORIES_TO_PATCH[@]//frameworks_opt_telephony} )
+fi
+
+echo ""
+for d in ${DIRECTORIES_TO_PATCH[@]}; do
+     ( [[ $(grep FAILED "$LOG_DIR"/"$d".log) != "" ]] && print_error "Failure in $d!" ) || echo "Patched $d succesfully"
+     [[ $(grep "Skip this patch?" "$LOG_DIR"/"$d".log) != "" ]] && print_error "Files not found! Examine ${LOG_DIR}/${LOG} because it is very likely you are using the wrong branch!"
+     cat "$LOG_DIR"/"$d".log >> "$LOG"
+     rm "$LOG_DIR"/"$d".log
+done
+
+cd "$ANDROID_HOME"; . build/envsetup.sh
 
 # to determine if Pdroid patches are applied or not
 touch $LOCK
